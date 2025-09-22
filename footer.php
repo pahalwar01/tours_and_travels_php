4f5f8367ac2f1f8bcbb06f46eeaf57d0d3bb4232
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-09-22 22:33:12 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <title>TMN Travels</title>
+  <title><?php include '../../brand_name.php'; ?></title>
 </head>
 <body>
   <footer>
