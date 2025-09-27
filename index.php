9b6cddb48a35148b217f2c399f9066e7c7875fa3
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-09-27 14:00:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <title><?php include '../../brand_name.php'; ?></title>
+  <title>Homepage</title>
    <style>
     body { font-family: Arial, sans-serif; margin:0; padding:0; 
         background:url('https://cdn.pixabay.com/photo/2024/01/18/10/07/sunset-8516639_1280.jpg') center/cover fixed no-repeat;
