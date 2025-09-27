39bf31dbf844d49de8233e867b9dc2fd9dd907e9
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-09-27 14:17:48 +0530

    Update header.php

--- header.php
+++ header.php
@@ -8,8 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <title><?php include 'brand_name.php'; ?></title>
-
+  <link rel="icon" type="image/x-icon" href="img/favicon.ico">
 </head>
 <body>
 
