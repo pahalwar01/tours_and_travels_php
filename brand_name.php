ba9d94a371ffbced82c299452a674885022b323b
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2026-09-05 15:44:47 +0530

    Update brand_name.php

--- brand_name.php
+++ brand_name.php
@@ -1,4 +1,8 @@
 <?php
- echo "TMN's Travel Agency";
-?>
+// brand_name.php
+if (!defined('SECURE_ACCESS')) {
+    header("HTTP/1.1 403 Forbidden");
+    exit("Direct access forbidden.");
+}
 
+echo htmlspecialchars("TMN's Travel Agency", ENT_QUOTES, 'UTF-8');
